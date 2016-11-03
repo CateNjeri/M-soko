6726f4989e30c89cee27a1fe861569dffa97853d
Aimen Khakwani and Elysia Nason <aimen+elysia@>
2016-11-03 11:33:01 -0700

    add beginnings of seasonal model hook

--- app/models/product.js
+++ app/models/product.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr(),
   description: DS.attr(),
-  seasonal: DS.attr(),
+  seasonal: DS.attr('boolean'),
   price: DS.attr('number'),
   image: DS.attr(),
   type: DS.attr(),
